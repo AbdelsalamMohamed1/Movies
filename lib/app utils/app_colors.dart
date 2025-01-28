bf32e3cbc2dfd0fa58597d1809939203bab132e2
Abdulsalam mohamed <abdulsalam0mhamed@gmail.com>
2025-01-28 22:39:41 +0200

    initialize the app with localization

---
+++ lib/app utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color red=Color(0xffe82626);
+  static const Color white=Color(0xffffffff);
+  static const Color white60 = Color(0x99ffffff);
+  static const Color black=Color(0xff121312);
+  static const Color yellow=Color(0xfff6bd00);
+  static const Color darkGray=Color(0xff282a28);
+  static const Color gray=Color(0xffadadad);
+}
---
+++ lib/app utils/app_textStyle.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+
+import 'package:movies/app%20utils/app_colors.dart';
+
+class AppTextstyle {
+  TextStyle regular20Black = TextStyle(
+      color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w400);
+  TextStyle semiBold20Black = TextStyle(
+      color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w600);
+  TextStyle regular20DarkGray = TextStyle(
+      color: AppColors.darkGray, fontSize: 20, fontWeight: FontWeight.w400);
+  TextStyle bold20Black = TextStyle(
+      color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w700);
+  TextStyle regular16DarkGray = TextStyle(
+      color: AppColors.darkGray, fontSize: 16, fontWeight: FontWeight.w400);
+  TextStyle bold20Gray = TextStyle(
+      color: AppColors.gray, fontSize: 20, fontWeight: FontWeight.w700);
+  TextStyle regular16White = TextStyle(
+      color: AppColors.white, fontSize: 16, fontWeight: FontWeight.w400);
+  TextStyle regular20White = TextStyle(
+      color: AppColors.white, fontSize: 20, fontWeight: FontWeight.w400);
+  TextStyle medium36White = TextStyle(
+      color: AppColors.white, fontSize: 36, fontWeight: FontWeight.w500);
+  TextStyle bold20White = TextStyle(
+      color: AppColors.white, fontSize: 20, fontWeight: FontWeight.w700);
+  TextStyle bold24White = TextStyle(
+      color: AppColors.white, fontSize: 24, fontWeight: FontWeight.w700);
+  TextStyle regular20White60 = TextStyle(
+      color: AppColors.white60, fontSize: 20, fontWeight: FontWeight.w400);
+  TextStyle regular14Yellow = TextStyle(
+      color: AppColors.yellow, fontSize: 14, fontWeight: FontWeight.w400);
+  TextStyle regular15Yellow = TextStyle(
+      color: AppColors.yellow, fontSize: 15, fontWeight: FontWeight.w400);
+  TextStyle regular16Yellow = TextStyle(
+      color: AppColors.yellow, fontSize: 16, fontWeight: FontWeight.w400);
+  TextStyle semiBold20Yellow = TextStyle(
+      color: AppColors.yellow, fontSize: 20, fontWeight: FontWeight.w600);
+  TextStyle bold20Yellow = TextStyle(
+      color: AppColors.yellow, fontSize: 20, fontWeight: FontWeight.w700);
+  TextStyle black14Yellow = TextStyle(
+      color: AppColors.yellow, fontSize: 14, fontWeight: FontWeight.w900);
+}
