95dc730c14fd8f5a688d39bff1cc28137cf15434
Abdulsalam mohamed <abdulsalam0mhamed@gmail.com>
2025-01-29 16:51:45 +0200

    Finished registerScreen's ui and some fixes in the initialization of the app

--- lib/app utils/app_textStyle.dart
+++ lib/app utils/app_textStyle.dart
@@ -3,40 +3,40 @@ import 'dart:ui';
 import 'package:movies/app%20utils/app_colors.dart';
 
 class AppTextstyle {
-  TextStyle regular20Black = TextStyle(
+  static TextStyle? regular20Black = TextStyle(
       color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w400);
-  TextStyle semiBold20Black = TextStyle(
+  static TextStyle? semiBold20Black = TextStyle(
       color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w600);
-  TextStyle regular20DarkGray = TextStyle(
+  static TextStyle? regular20DarkGray = TextStyle(
       color: AppColors.darkGray, fontSize: 20, fontWeight: FontWeight.w400);
-  TextStyle bold20Black = TextStyle(
+  static TextStyle? bold20Black = TextStyle(
       color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w700);
-  TextStyle regular16DarkGray = TextStyle(
+  static TextStyle? regular16DarkGray = TextStyle(
       color: AppColors.darkGray, fontSize: 16, fontWeight: FontWeight.w400);
-  TextStyle bold20Gray = TextStyle(
+  static TextStyle? bold20Gray = TextStyle(
       color: AppColors.gray, fontSize: 20, fontWeight: FontWeight.w700);
-  TextStyle regular16White = TextStyle(
+  static TextStyle? regular16White = TextStyle(
       color: AppColors.white, fontSize: 16, fontWeight: FontWeight.w400);
-  TextStyle regular20White = TextStyle(
+  static TextStyle? regular20White = TextStyle(
       color: AppColors.white, fontSize: 20, fontWeight: FontWeight.w400);
-  TextStyle medium36White = TextStyle(
+  static TextStyle? medium36White = TextStyle(
       color: AppColors.white, fontSize: 36, fontWeight: FontWeight.w500);
-  TextStyle bold20White = TextStyle(
+  static TextStyle? bold20White = TextStyle(
       color: AppColors.white, fontSize: 20, fontWeight: FontWeight.w700);
-  TextStyle bold24White = TextStyle(
+  static TextStyle? bold24White = TextStyle(
       color: AppColors.white, fontSize: 24, fontWeight: FontWeight.w700);
-  TextStyle regular20White60 = TextStyle(
+  static TextStyle? regular20White60 = TextStyle(
       color: AppColors.white60, fontSize: 20, fontWeight: FontWeight.w400);
-  TextStyle regular14Yellow = TextStyle(
+  static TextStyle? regular14Yellow = TextStyle(
       color: AppColors.yellow, fontSize: 14, fontWeight: FontWeight.w400);
-  TextStyle regular15Yellow = TextStyle(
+  static TextStyle? regular15Yellow = TextStyle(
       color: AppColors.yellow, fontSize: 15, fontWeight: FontWeight.w400);
-  TextStyle regular16Yellow = TextStyle(
+  static TextStyle? regular16Yellow = TextStyle(
       color: AppColors.yellow, fontSize: 16, fontWeight: FontWeight.w400);
-  TextStyle semiBold20Yellow = TextStyle(
+  static TextStyle? semiBold20Yellow = TextStyle(
       color: AppColors.yellow, fontSize: 20, fontWeight: FontWeight.w600);
-  TextStyle bold20Yellow = TextStyle(
+  static TextStyle? bold20Yellow = TextStyle(
       color: AppColors.yellow, fontSize: 20, fontWeight: FontWeight.w700);
-  TextStyle black14Yellow = TextStyle(
+  static TextStyle? black14Yellow = TextStyle(
       color: AppColors.yellow, fontSize: 14, fontWeight: FontWeight.w900);
 }
